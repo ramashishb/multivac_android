293db920a6c81656d5a2356e37850416feda6f22
Ramashish Baranwal <ramashish.baranwal@flipkart.com>
2015-06-05 21:35:38 +0530

    Showing Actions for an Event in dialog

--- Multivac/app/src/main/java/multivac/com/multivac/EventListFragment.java
+++ Multivac/app/src/main/java/multivac/com/multivac/EventListFragment.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -30,6 +31,37 @@ public class EventListFragment extends Fragment {
                 android.R.layout.simple_list_item_1, events);
         eventlistView.setAdapter(arrayAdapter);
 
+        eventlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Event event = events.get(position);
+                List<Act> acts = event.getActs();
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                LayoutInflater layoutInflater = getActivity().getLayoutInflater();
+                final View dialogView = layoutInflater.inflate(R.layout.dialog_show_event_acts, null);
+                final AlertDialog.Builder dialog = builder.setView(dialogView);
+                ArrayAdapter<Act> actAdapter = new ArrayAdapter<Act>(getActivity(),
+                        android.R.layout.simple_list_item_1, acts);
+                ListView actsView = (ListView) dialogView.findViewById(R.id.event_acts);
+                actsView.setAdapter(actAdapter);
+                dialog.setTitle("Actions");
+                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Dismiss
+                    }
+                });
+                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Dismiss
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
         View view = rootView.findViewById(R.id.event_add);
         view.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +71,7 @@ public class EventListFragment extends Fragment {
                 final View dialogView = layoutInflater.inflate(R.layout.dialog_add_event, null);
                 final AlertDialog.Builder dialog = builder.setView(dialogView);
 
-                dialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                dialog.setPositiveButton("Add Event", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         String title = ((EditText) dialogView.findViewById(R.id.title)).getText().toString();
