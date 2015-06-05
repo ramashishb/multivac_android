86b13ec825358c664b23ef0fe6ae8fbed3070860
shashwat <shashwat@flipkart.com>
2015-06-06 03:10:21 +0530

    Making events more stable and push based

--- device/multivac_bt_sketch/multivac_bt_sketch.ino
+++ device/multivac_bt_sketch/multivac_bt_sketch.ino
@@ -7,28 +7,37 @@
 #include <LiquidCrystal.h>
 #include <SoftwareSerial.h>
 
+#define DEVICE_PROFILE "car"
 
 #define ECHOPIN 7        // Pin to receive echo pulse 
 #define TRIGPIN 6        // Pin to send trigger pulse
 
-#define TOWARDS "towards"
-#define AWAY "away"
-#define STATIONARY "stationary"
+#define EVENT_MOVE_TOWARDS "towards"
+#define EVENT_MOVE_AWAY "away"
+#define EVENT_STATIONARY "stationary"
+
+#define STATE_MOTION_NONE "none"
+#define STATE_MOTION_AWAY "moving_away"
+#define STATE_AWAY "away"
+#define STATE_MOTION_TOWARDS "moving_towards"
+#define STATE_NEAR "near"
 
 #define STATIONARY_THRESHOLD 10
 
-boolean isDebug = false;
-
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
-
 SoftwareSerial mySerial(8, 9); // RX, TX
 
+// states
 int command = 0;
+
+//float distanceStateStart = 0.0;
 float distance = 0.0;
-String direction = STATIONARY;
+
+String motionState = STATE_MOTION_NONE;
 int stationaryCount = 0;
 
+float referenceDistance = 0;
 void setup() {
   lcd.begin(16, 2);
   lcd.print("Multivac On!");
@@ -39,6 +48,7 @@ void setup() {
 
   lcd.clear();
   lcd.print("Multivac On!");
+
 }
 
 int getDistance() {
@@ -50,53 +60,106 @@ int getDistance() {
   digitalWrite(TRIGPIN, LOW);
   // Distance Calculation
 
-  float distance = pulseIn(ECHOPIN, HIGH);
-  distance = distance / 58;
+  float distanceL = pulseIn(ECHOPIN, HIGH);
+  distanceL = distanceL / 58;
 
-  return distance;
+  return distanceL;
 }
 
 void loop() {
-  lcd.setCursor(0, 1 );
+  if (referenceDistance == 0) {
+      distance = referenceDistance = getDistance();
+  }
+  
   float currDistance = getDistance();
-  float diffDistance = (distance - currDistance);
-//  String direction = STATIONARY;
 
-  float changePercent = abs(diffDistance) * 100.0 / distance;
-  if (changePercent > 5.0) {
+  float deltaMoved = (distance - currDistance);
+  float absDistance = abs(deltaMoved);
+
+  String newState = motionState;
+  if (absDistance > 10.0 && currDistance < referenceDistance) {
+    // so the person moved
     distance = currDistance;
-    if ( diffDistance > 0 )
-      direction = TOWARDS;
-    else
-      direction = AWAY;
-  } else {
+
+    if ( motionState != STATE_NEAR && deltaMoved > 0 ) {
+      newState = STATE_MOTION_TOWARDS;
+    } else if (motionState != STATE_AWAY && deltaMoved < 0) {
+      newState = STATE_MOTION_AWAY;
+    }
+  } else if (absDistance < 5.0) {
     if (++stationaryCount > STATIONARY_THRESHOLD) {
-      direction = STATIONARY;
+      newState = STATE_MOTION_NONE;
       stationaryCount = 0;
     }
   }
 
-  //if some date is sent, reads it and saves in state
+  ////////////////////
+  //Send a response if we have received an explicit command to send message or change
+  bool shouldSendEvent = false;
+  String eventToSend = "";
+
+  // check if there was an explicit command to send
   if (mySerial.available() > 0) {
     command = mySerial.read();
     if (command != 0) {
-      String response;
-      if (command == 1 || command == 'P' || command == 'p') {
-        response = "car";
-      } else {
-        response = "NaN";
-      }
-
-      response += ",";
-      response += direction;
-      response += "\n";
-      mySerial.print(response);
-    }
-  } else {
-    if (isDebug) {
-      mySerial.println(direction);
+      shouldSendEvent = (command == 1 || command == 'P' || command == 'p');
     }
   }
+
+  if (newState == motionState) {
+    // check if moved too far or too close,
+    if (motionState == STATE_MOTION_TOWARDS && currDistance <= 100.0) {
+      motionState = STATE_NEAR;
+      shouldSendEvent = true;
+    } else if (motionState == STATE_MOTION_AWAY && currDistance > 100.0)   {
+      motionState = STATE_AWAY;
+      shouldSendEvent = true;
+    }
+  } else {
+    // new direction or stop
+    motionState = newState;
+    shouldSendEvent = true;
+  }
+
+
+  if (shouldSendEvent) {
+    if (motionState == STATE_AWAY) {
+      eventToSend = EVENT_MOVE_AWAY;
+    } else if (motionState == STATE_NEAR) {
+      eventToSend = EVENT_MOVE_TOWARDS;
+    }      else {
+      eventToSend = EVENT_STATIONARY;
+    }
+    String response = DEVICE_PROFILE;
+    response += ",";
+    response += eventToSend;
+    response += "\n";
+    mySerial.print(response);
+  }
+
+
+
+  //if some date is sent, reads it and saves in state
+  //  if (mySerial.available() > 0) {
+  //    command = mySerial.read();
+  //    if (command != 0) {
+  //      String response;
+  //      if (command == 1 || command == 'P' || command == 'p') {
+  //        response = "car";
+  //      } else {
+  //        response = "NaN";
+  //      }
+  //
+  //      response += ",";
+  //      response += direction;
+  //      response += "\n";
+  //      mySerial.print(response);
+  //    }
+  //  } else {
+  //    if (isDebug) {
+  //      mySerial.println(direction);
+  //    }
+  //  }
 
 
   String line = "Cmd:";
@@ -104,6 +167,7 @@ void loop() {
   line += " d:";
   line += int(distance);
 
+  lcd.setCursor(0, 1 );
   int written = lcd.print(line);
   while (written <= 16) {
     written += lcd.print(" ");
